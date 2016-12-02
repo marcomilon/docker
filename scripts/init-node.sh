2dc7d468fcf49d91f0ca668af14fecc6484f6715
Marco Milon <marco.milon@gmail.com>
2016-12-02 07:51:16 -0500

    added node-inspector, npm and nodemon

--- scripts/init-node.sh
+++ scripts/init-node.sh
@@ -2,9 +2,9 @@
 
 BASEDIR=$(dirname "$0")
 
-docker build -f $BASEDIR/NodeDockerfile -t idt/node:grunt-cli .
+docker build -f $BASEDIR/DockerfileNode -t idt/node:grunt-cli .
 
 echo "Clean up build image"
-docker rmi node:4.6.1
+docker rmi node:6.9.1-alpine
 echo
 echo "done"
