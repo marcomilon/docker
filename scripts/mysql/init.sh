82c0edaf6561dd4b2f13dddf1fa78cd54137e357
Marco Milon <marco.milon@gmail.com>
2017-05-18 12:12:59 -0500

    New version

--- scripts/mysql/init.sh
+++ scripts/mysql/init.sh
@@ -2,14 +2,6 @@
 
 BASEDIR=$(dirname "$0")
 
-read -p "Would you like to pull the lastest mysql container? [Y/n] " mysql
-if [[ $mysql == n* ]]; then
-  echo "Bye bye."
-  exit;
-fi
-
-echo "Pulling Mysql"
-
 if [ -z "$IDT_MYSQL_DATA_DIR" ]; then
     echo
     echo "Please set the environment variable IDT_MYSQL_DATA_DIR to point the mysql datadir directory on host machine."
@@ -27,13 +19,13 @@ if [ -z "$IDT_MYSQL_PWD" ]; then
   fi
 
 echo
-echo "Everything looks good... Pullling images"
+echo "Everything looks good... Pullling latest image"
 echo
 
-sh $BASEDIR/pull-mysql.sh
+docker pull mysql:latest
 
 echo
 echo "Mysql datadir: $IDT_MYSQL_DATA_DIR"
-echo "Mysql host: idt-mysql";
-echo "Mysql username: root";
+echo "Mysql host: idt-mysql"
+echo "Mysql username: root"
 echo "Mysql password: $IDT_MYSQL_PWD"
