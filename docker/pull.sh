deca2cbe9a52765ca66e6cd7747755c5ea9a84cb
Marco Milon <marco.milon@gmail.com>
2017-11-20 15:38:42 -0500

    New version

---
+++ docker/pull.sh
@@ -0,0 +1,3 @@
+docker pull php:7.0-apache-jessie
+docker pull mysql
+docker pull phpmyadmin/phpmyadmin
---
+++ fs/initenv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if test "$#" -ne 1; then
+    echo "student name is required"
+    exit 1
+fi
+
+mkdir -p php/students/$1/examples
+mkdir -p php/students/$1/curso
+cp php/code/* php/students/$1/examples 
+cp php/code/* php/students/$1/curso
+(export DOCUMENT_ROOT="$(pwd)/php/students/$1/curso/" && docker-compose -f ../docker/lamp/docker-compose.yml up -d)
