7ef5b3b330e48ab0b29e6894afbdf76db6d06c48
Marco Milon <marco.milon@gmail.com>
2017-11-26 21:08:29 -0500

    New serve.sh

--- bin/serve.sh
+++ bin/serve.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(dirname "$0")
 
-(export DOCUMENT_ROOT="$(pwd)" && docker-compose -f $BASEDIR/../docker/lamp/ap/docker-compose.yml up -d)
+(export DOCUMENT_ROOT="$(pwd)" && docker-compose -f $BASEDIR/../docker/lamp/ap/docker-compose.yml up)
