f2196425e199324b53cf205bab02c5eb706c2086
Marco Milon <marco.milon@gmail.com>
2017-12-14 16:05:22 -0500

    New lamp

--- bin/serve.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-BASEDIR=$(dirname "$0")
-
-(export DOCUMENT_ROOT="$(pwd)" && docker-compose -f $BASEDIR/../docker/lamp/ap/docker-compose.yml up)
--- docker/lamp/ap/initenv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-(export DOCUMENT_ROOT="$(pwd)/php/students/$1/curso/" && docker-compose -f ../docker/lamp/fs/docker-compose.yml up -d)
